b2fc807819a50d85314df3b343b2401aaeb7034a
Mahdi <mahdi57.kamali1@gmail.com>
2024-05-24 19:37:41 +0330

    Add match single regex skill

--- skills/match_regex.py
+++ skills/match_regex.py
@@ -7,10 +7,19 @@ sys.path.append(os.getcwd())
 
 from regex_classifier import *
 
-class MatchRegex(Skill):
-    @match_regex(r'^match ')
+class MatchAll(Skill):
+    @match_regex(r'^match')
     async def match(self, message):
-        print("---RECEIVED MATCH COMMAND---")
+        print("---RECEIVED MATCH-ALL COMMAND---")
         regex_clf = RegexClassifier()
         matches = regex_clf.match_patterns(message.text.split()[1])
+        await message.respond(str(matches))
+
+class MatchRegex(Skill):
+    @match_regex(r'^match_regex')
+    async def match(self, message):
+        print("---RECEIVED MATCH-REGEX COMMAND---")
+        regex_clf = RegexClassifier()
+        cmd = message.text.split()
+        matches = regex_clf.match_input_pattern(cmd[1], cmd[2])
         await message.respond(str(matches))
