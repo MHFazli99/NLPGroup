27424990aa8b098e27e45e3a181a63582a6237c1
Mahdi <mahdi57.kamali1@gmail.com>
2024-05-24 17:22:42 +0330

    Add welcome message

--- skills/accept_invite.py
+++ skills/accept_invite.py
@@ -1,12 +1,23 @@
 from opsdroid.skill import Skill
 from opsdroid.matchers import match_event
-from opsdroid.events import UserInvite, JoinRoom
+from opsdroid.events import UserInvite, JoinRoom, Message
+
+
+WELCOME_MESSAGE = '''
+سلام! این بازوی دسته‌بند رجکس است. شما می‌توانید چندین کار را اینجا انجام دهید:
+1. match "text"
+با وارد کردن این دستور می‌توانید ایمیل‌ها، شماره‌تلفن‌ها و آدرس‌های موجود در text را استخراج کنید.
+'''
 
 
 class AcceptInvites(Skill):
     @match_event(UserInvite)
     async def user_invite(self, invite):
-        print("\n ---USER INVITE---\n") 
-        print(f"user invite --> {invite}")
+        print("\n ---USER INVITE---\n")
         if isinstance(invite, UserInvite):
             await invite.respond(JoinRoom())
+    @match_event(JoinRoom)
+    async def send_welcome_message(self, event):
+        print("\n ---JOIN ROOM---\n")
+        if isinstance(event, JoinRoom):
+            await event.respond(Message(WELCOME_MESSAGE))
